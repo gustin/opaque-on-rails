c86fa1cc9a910fe099708d1c5e80fe2e10d4be9b
gustin <gustin@users.noreply.github.com>
2019-12-18 00:09:10 -0500

    Flush out authentication and registration controller (should rename).

--- app/controllers/plaintext/rails/authenticate_controller.rb
+++ app/controllers/plaintext/rails/authenticate_controller.rb
@@ -1,8 +1,23 @@
 require 'plaintext'
 class AuthenticateController < ApplicationController
   def start
+    puts '===> Authentication Start'
+    params = JSON.parse(request.raw_post)
+    puts params
+    beta, v, envelope, key, y = Plaintext::Authentication::start(params["username"], params["alpha"], params["key"])
+
+    puts "Key: #{key}"
+
+    render json: { beta: beta, v: v, envelope: envelope, key: key, y: y }
   end
 
   def finalize
+    puts '===> Authentication Login'
+    params = JSON.parse(request.raw_post)
+    puts params
+    Plaintext::Authentication::finalize(params["username"], params["key"], params["x"])
+
+    # return Plaintext::Paseto Session Token, store in database, etc
+    render json: { logged: 'in' }
   end
 end
--- app/controllers/plaintext/rails/register_controller.rb
+++ app/controllers/plaintext/rails/register_controller.rb
@@ -1,8 +1,22 @@
 require 'plaintext'
 class RegisterController < ApplicationController
   def start
+    puts '====> Regsistration Start'
+    params = JSON.parse(request.raw_post)
+    puts params
+    beta, v, pub_s = Plaintext::Registration::start(params["username"], params["alpha"])
+    puts "******************************"
+    puts  "Beta: #{beta}"
+
+    render json: { beta: beta, v: v, pub_s: pub_s }
   end
 
   def finalize
+    puts '====> Registration Final'
+    params = JSON.parse(request.raw_post)
+    puts params
+    Plaintext::Registration::finalize(params["username"], params["pub_u"], params["auth_env"])
+
+    render json: { final: "hurrah" }
   end
 end
