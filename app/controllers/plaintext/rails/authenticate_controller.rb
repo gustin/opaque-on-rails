f6f1960856b05cfde1aaf267143ebf010c4b9abe
gustin <gustin@users.noreply.github.com>
2020-01-13 15:14:13 -0500

    Route to confirm second factors.

--- app/controllers/plaintext/rails/authenticate_controller.rb
+++ app/controllers/plaintext/rails/authenticate_controller.rb
@@ -12,7 +12,17 @@ class Plaintext::Rails::AuthenticateController < ApplicationController
     render json: { beta: beta, v: v, envelope: envelope, key: key, y: y }
   end
 
-  def second_factor
+  def confirm_second_factor
+    params = JSON.parse(request.raw_post)
+    puts params
+    confirmed = Plaintext::Authenticator::confirm_second_factor(
+      params["username"], params["code"]
+    )
+    puts confirmed
+    render json: { confirmed: confirmed }
+  end
+
+  def generate_second_factor
     params = JSON.parse(request.raw_post)
     puts params
     challenge = Plaintext::Authenticator::generate_second_factor(
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Plaintext::Rails::Engine.routes.draw do
   post 'authenticate/start'
-  post 'authenticate/second_factor'
+  post 'authenticate/confirm_second_factor'
   post 'authenticate/finalize'
+  post 'authenticate/generate_second_factor'
   post 'register/start'
   post 'register/finalize'
 end
